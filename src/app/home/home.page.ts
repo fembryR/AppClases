da552092ef81c4e975325b8fb2c89b710fb65dcd
Felipe Embry <fe.embry@duocuc.cl>
2024-08-28 21:01:54 -0400

    Segundo commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+
+import { Component, ElementRef, ViewChildren, ViewChild } from '@angular/core';
+import type { Animation, IonModal } from '@ionic/angular';
+import { AnimationController, IonCard } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonCard, { read: ElementRef })
+  card!: ElementRef<HTMLIonCardElement>;
+  @ViewChild('modal', { static: true })
+  modal!: IonModal;
+  
+  private animation!: Animation;
 
-  constructor() {}
+  constructor(private animationCtrl: AnimationController) {}
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(this.card.nativeElement)
+      .duration(1500)
+      .iterations(Infinity)
+      .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+      .fromTo('opacity', '1', '0.2');
+  }
+
+  play() {
+    this.animation.play();
+  }
+
+  pause() {
+    this.animation.pause();
+  }
+
+  stop() {
+    this.animation.stop();
+  }
 
 }
+
